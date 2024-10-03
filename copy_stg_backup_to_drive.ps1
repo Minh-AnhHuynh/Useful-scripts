e1601d5f627f491c5acec3649530438638755c69
Minh-AnhHuynh <minh-anh.huynh@etu.u-paris.fr>
2024-10-03 15:59:22 +0200

    fixed duplicate files for stg + folder and clean feature

--- copy_stg_backup_to_drive.ps1
+++ copy_stg_backup_to_drive.ps1
@@ -7,7 +7,7 @@ if ($process) {
     Start-Process "C:\Program Files\Google\Drive File Stream\68.0.2.0\GoogleDriveFS.exe"
 
     while (!(Get-Process GoogleDriveFS -ErrorAction SilentlyContinue)) {
-        Start-Sleep -Seconds 1
+        Start-Sleep -Seconds 2
     }
     Write-Output "Google Drive has started successfully."
 }
@@ -30,30 +30,56 @@ $files = Get-ChildItem -Path $folder.FullName
 
 
 if ($null -eq $files) { 
-    # Folder does not exist
+    # Files does not exist
     Write-Output "Files does not exist"
     exit
 }
-# Get the last file to backu^p
-$backup_files = $files[-1]
+# Get the last file to backup
+$backup_files = $files | Sort-Object LastWriteTime -Descending | Select-Object -First 1
 
+# Get the computer name
+$computerName = $env:COMPUTERNAME
+# Convert the computer name to CamelCase
 
+$camelCaseComputerName = -join ($computerName -split '[_ ]' | ForEach-Object { 
+    if ($_ -eq '-') { 
+        $_ 
+    } else { 
+        $_.Substring(0,1).ToUpper() + $_.Substring(1).ToLower() 
+    } 
+})
 
+# Define the destination path
+$destination_backup = "G:\My Drive\Backup\Simple Tab Groups"
+
+# Create a unique folder name based on the computer name
+$uniqueFolderName = Join-Path -Path $destination_backup -ChildPath $camelCaseComputerName
+
+# Ensure the unique folder exists
+if (-not (Test-Path -Path $uniqueFolderName)) {
+    New-Item -Path $uniqueFolderName -ItemType Directory | Out-Null
+}
+
+# Update the destination to include the unique folder
+$destination = $uniqueFolderName
+# Copy the file to the destination with error handling
 try {
-    $destination = "G:\My Drive\Backup\Simple Tab Groups"
-    Move-Item -Path $files.FullName -Destination $destination
-} catch [System.IO.IOException] {
-    Write-Output "Error: The file already exists at the destination."
-    break
+    Copy-Item -Path $backup_files.FullName -Destination $destination -ErrorAction Stop
+    Write-Output "File backed up: $($backup_files.Name)"
 } catch {
-    Write-Output "Error: The destination path was not found."
-    break
+    Write-Output "Error copying file: $_"
+}
+
+# Do post backup cleanup of local files
+
+# Remove all folders except the most recent one
+$folders | Where-Object { $_.FullName -ne $folder.FullName } | ForEach-Object {
+    Remove-Item -Path $_.FullName -Recurse -Force
+    Write-Output "Removed folder: $($_.FullName)"
 }
 
 
-
-Write-Output "File backed up: $($backup_files.Name)"
-
+# Show a message box to the user
 Add-Type -AssemblyName System.Windows.Forms
 Add-Type -AssemblyName PresentationFramework
 [System.Windows.MessageBox]::Show("STG Backup completed successfully!", "Backup Status", [System.Windows.Forms.MessageBoxButtons]::OK)
